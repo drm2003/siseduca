bcae32c0897dd9e5ea0ca4e204200bd7e20afe1b
Daniel Rodrigues Monteiro <drm2003@ig.com.br>
2016-05-24 22:10:30 -0300

    alterações

--- arquillian/src/test/java/br/com/cdan/GreeterTest.java
+++ arquillian/src/test/java/br/com/cdan/GreeterTest.java
@@ -1,20 +1,32 @@
 package br.com.cdan;
 
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class GreeterTest {
+	@Inject
+	Greeter greeter;
 
 	@Deployment
 	public static JavaArchive createDeployment() {
-		return ShrinkWrap.create(JavaArchive.class).addClass(Greeter.class).addAsManifestResource(EmptyAsset.INSTANCE,
-				"beans.xml");
+		JavaArchive jar = ShrinkWrap.create(JavaArchive.class).addClass(Greeter.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+		System.out.println(jar.toString(true));
+		return jar;
 	}
 
 	@Test
 	public void should_create_greeting() {
-		Assert.fail("Not yet implemented");
+		Assert.assertEquals("Hello, Earthling!", greeter.createGreeting("Earthling"));
+		greeter.greet(System.out, "Earthling");
 	}
 }
